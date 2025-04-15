04f341483ca31dcc4128d483fc62ee0131b26abe
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2025-04-15 19:53:21 +0200

    really merging the whitebox

--- whitebox_rs/src/main.rs
+++ whitebox_rs/src/main.rs
@@ -1,4 +1,3 @@
-mod consts;
 mod decrypt;
 mod encrypt;
 mod lattice;
@@ -10,13 +9,10 @@ use numpy::ndarray::s;
 use serde_json::json;
 use serde_json::Result;
 use std::fs::File;
-use chrono::Utc;
 
 use decrypt::decrypt_json;
-
-fn load_public_key(dir: &str) -> serde_json::Result<(NTRUVector, NTRUVector, usize, i64)> {
-    let pub_enc_data_file = File::open(format!("{}/pub_enc_data.json", dir))
-        .map_err(serde_json::Error::io)?;
+fn load_public_key() -> serde_json::Result<(NTRUVector, NTRUVector, usize, i64)> {
+    let pub_enc_data_file = File::open("pub_enc_data.json").map_err(serde_json::Error::io)?;
     let data: encrypt::PubEncData = serde_json::from_reader(pub_enc_data_file)?;
 
     let degree = data.degree;
@@ -38,19 +34,14 @@ fn load_public_key(dir: &str) -> serde_json::Result<(NTRUVector, NTRUVector, usi
 }
 
 fn main() -> Result<()> {
-    let timestamp = Utc::now().format("%Y%m%d%H%M%S").to_string();
-    let dir_name = format!(".rpack-whitebox-{}", timestamp);    
-    std::fs::create_dir(&dir_name).expect("Failed to create timestamped directory");
-    println!("Using directory: {}", dir_name);
-
-
-    create_wb::create_wb(&dir_name);
+    create_wb::create_wb();
 
     let message_to_encrypt = "MEOW MEOW MEOW!!";
     println!("Message to encrypt: {}", message_to_encrypt);
 
+    // --- Encryption Section ---
     println!("\n---Encryption---");
-    let (pka, pkb, degree, modulus) = load_public_key(&dir_name)?;
+    let (pka, pkb, degree, modulus) = load_public_key()?;
     let message_bytes = message_to_encrypt.as_bytes();
     let mut message_bits_vec = Vec::new();
     for byte in message_bytes {
@@ -58,7 +49,7 @@ fn main() -> Result<()> {
             message_bits_vec.push((byte >> (7 - i)) & 1);
         }
     }
-    let message_bits = Array1::from_vec(message_bits_vec.iter().map(|&x| x as i64).collect::<Vec<i64>>());
+    let message_bits = Array1::from_vec(message_bits_vec.iter().map(|&x| x as i64).collect::<Vec<i64>>()); // Removed mut
 
     if message_bits.len() > degree {
         println!("Error: Message too long for encryption.");
@@ -70,8 +61,7 @@ fn main() -> Result<()> {
 
     let (a1, a2) = encrypt::encrypt_func(&message_padded, &pka, &pkb, degree, modulus);
 
-    let ciphertext_file = File::create(format!("{}/ciphertext.json", dir_name))
-        .map_err(serde_json::Error::io)?;
+    let ciphertext_file = File::create("ciphertext.json").map_err(serde_json::Error::io)?;
     let ciphertext_data = json!({
         "a1": a1.vector.to_vec(),
         "a2": a2.vector.to_vec(),
@@ -79,10 +69,26 @@ fn main() -> Result<()> {
 
     serde_json::to_writer_pretty(ciphertext_file, &ciphertext_data)?;
 
-    println!("Message encrypted and saved to {}/ciphertext.json", dir_name);
+    println!("Message encrypted and saved to ciphertext.json");
 
+    // --- Decryption Section ---
     println!("\n---Decryption---");
-    decrypt_json(&dir_name)?;
-    std::fs::remove_dir_all(&dir_name).expect("Failed to remove directory");
+    decrypt_json()?;
+
+    // -- Remove generated json files --
+    println!("\n---Cleaning---");
+    std::fs::remove_file("ciphertext.json").unwrap_or_else(|_| {
+        println!("Error removing ciphertext.json");
+    });
+    std::fs::remove_file("private_data.json").unwrap_or_else(|_| {
+        println!("Error removing private_data.json");
+    });
+    std::fs::remove_file("pub_enc_data.json").unwrap_or_else(|_| {
+        println!("Error removing pub_enc_data.json");
+    });
+    std::fs::remove_file("wb_dec_data.json").unwrap_or_else(|_| {
+        println!("Error removing wb_dec_data.json");
+    });
+
     Ok(())
 }
