a17730084dc8d8aba26ca788adac7067513ad496
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2025-04-16 14:46:52 +0200

    MONKE

---
+++ rpack/stub/src/BIGMONKE.c
@@ -0,0 +1,42 @@
+/*
+Obfuscate using Tigress: https://tigress.wtf/index.html
+---
+
+$ tigress  --Environment=x86_64:Linux:Gcc:4.6 \
+    --Transform=InitBranchFuns --InitBranchFunsCount=1 \
+    --Transform=InitEntropy \
+    --Transform=InitImplicitFlow --Functions=main \
+    --Transform=InitOpaque --Functions=main  --InitOpaqueStructs=list,array,env \
+    --Transform=Virtualize --Functions=main \
+         --VirtualizeAddOpaqueToVPC=true --VirtualizeImplicitFlowPC=PCInit, PCUpdate \
+         BIGMONKE.c --out=TigerMONKE.c && gcc TigerMONKE.c -static -s -o BIGMONKE
+*/
+
+#include <stdio.h>
+// Tiger dependencies
+#include<time.h>
+#include<stdlib.h>
+
+
+int main() {
+    const char bigMonke[] = 
+        "            ,.-\" \"-.,\n"
+        "           /   ===   \\\n"
+        "          /  =======  \\\n"
+        "       __|  (o)   (0)  |__\n"
+        "      / _|    .---.    |_ \\\n"
+        "     | /.----/ O O \\----.\\ |\n"
+        "      \\/     |     |     \\/\n"
+        "      |                   |\n"
+        "      |                   |\n"
+        "      |                   |\n"
+        "      _\\   -.,_____,.-   /_\n"
+        "  ,.-\"  \"-.,_________,.-\"  \"-.,\n"
+        " /         |   BIG   |         \\\n"
+        "|          l. MONKE .l          |\n";
+    printf("%s", bigMonke);
+    int *p = NULL;
+    *p = 0;
+    printf("%d", *p);
+    return 0;
+}
